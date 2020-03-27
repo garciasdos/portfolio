10114748a85e8ef9f0240ccdd532230e5fdad33c
Diego Garcia <garciasdos@gmail.com>
2020-03-27 21:27:04 +0100

    Updates

---
+++ precache-manifest.fca5df4358b65b6fd9b3ef8b4345c1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7420f0dd0be7e86ef4053883c3784839",
+    "url": "/en/index.html"
+  },
+  {
+    "revision": "0cd72436262db17b652e",
+    "url": "/en/static/css/main.f464fb4f.chunk.css"
+  },
+  {
+    "revision": "cb7c9b82d5fc510885a4",
+    "url": "/en/static/js/2.be3207f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/en/static/js/2.be3207f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd72436262db17b652e",
+    "url": "/en/static/js/main.38cfb6b1.chunk.js"
+  },
+  {
+    "revision": "5f6f61d2145cc9adf5c1",
+    "url": "/en/static/js/runtime-main.e81f2ab6.js"
+  }
+]);

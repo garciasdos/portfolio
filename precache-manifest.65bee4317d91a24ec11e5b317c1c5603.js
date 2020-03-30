80157867cfa9149b6994eb998e3140d05155555b
Diego Garcia <garciasdos@gmail.com>
2020-03-30 13:59:14 +0200

    Updates

---
+++ precache-manifest.65bee4317d91a24ec11e5b317c1c5603.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264d4200bcac74156223b88a0bc7a14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b78b12fe5d58ab11e71",
+    "url": "/static/css/main.aae2ac35.chunk.css"
+  },
+  {
+    "revision": "ac8bdd195f0ff9189790",
+    "url": "/static/js/2.9140db13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9140db13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b78b12fe5d58ab11e71",
+    "url": "/static/js/main.17914ae6.chunk.js"
+  },
+  {
+    "revision": "4ca9c42e1914a4c19d35",
+    "url": "/static/js/runtime-main.39ce593c.js"
+  },
+  {
+    "revision": "baab9e546e46e5dc039cc45acdc78a44",
+    "url": "/static/media/avatar.baab9e54.jpeg"
+  },
+  {
+    "revision": "bd34f9ea36ee6ad198ce018bc1168a1a",
+    "url": "/static/media/holaluz-logo.bd34f9ea.svg"
+  },
+  {
+    "revision": "1509d8066f9f688726e3982915317d42",
+    "url": "/static/media/logo-multihelpers2.1509d806.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.395b1846f6ac40fc424c4b153d7c972e.js"
+  "/precache-manifest.65bee4317d91a24ec11e5b317c1c5603.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
